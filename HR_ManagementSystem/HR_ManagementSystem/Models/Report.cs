4aa23fe2208ce8be45e1b4e3eaa73bcf2e9ae556
Andrei <postolacheandrei182@gmail.com>
2023-03-20 17:23:26 +0200

    baza de date functionala

--- HR_ManagementSystem/HR_ManagementSystem/Models/Report.cs
+++ HR_ManagementSystem/HR_ManagementSystem/Models/Report.cs
@@ -7,10 +7,11 @@ namespace HR_ManagementSystem.Models
         public string ReportId { get; set; }
         public string ReportName { get; set; }
 
-
+        public string ReportType { get; set; }
+        public DateTime ReportDate { get; set; }   
 
         [ForeignKey("Employee")]
-        public int? EmployeeId { get; set; }  // relatie one-to-many - un raport poate facut pentru mai multi employee (ex. nr de zile libere ramase pt toti employee)
-        public ICollection<Employee>? Employees { get; set; }
+        public virtual int? EmployeeId { get; set; }  // relatie one-to-many - un raport poate facut pentru mai multi employee (ex. nr de zile libere ramase pt toti employee)
+        public virtual  ICollection<Employee>? Employees { get; set; }
     }
 }
