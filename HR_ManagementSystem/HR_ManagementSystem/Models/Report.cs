8a174cdb137806545331a71a0c2e2e7873e1c433
Deni <denisa.obreja@yahoo.com>
2023-03-21 12:14:10 +0200

    changed some models

--- HR_ManagementSystem/HR_ManagementSystem/Models/Report.cs
+++ HR_ManagementSystem/HR_ManagementSystem/Models/Report.cs
@@ -4,14 +4,5 @@ namespace HR_ManagementSystem.Models
 {
     public class Report
     {
-        public string ReportId { get; set; }
-        public string ReportName { get; set; }
-
-        public string ReportType { get; set; }
-        public DateTime ReportDate { get; set; }   
-
-        [ForeignKey("EmployeeId")]
-        public virtual int? EmployeeId { get; set; }  // relatie one-to-many - un raport poate facut pentru mai multi employee (ex. nr de zile libere ramase pt toti employee)
-        public virtual  ICollection<Employee>? Employees { get; set; }
     }
 }
